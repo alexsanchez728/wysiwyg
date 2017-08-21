671f6f63255f6161440e222f0d828eef539cb9df
Alex Sanchez <alesanchez728@gmail.com>
2017-08-21 18:52:14 -0500

    ready to add dotted border step

--- wysiwyg.js
+++ wysiwyg.js
@@ -55,22 +55,21 @@ var person = [{
     death: 1797
   }
 }];
-console.log(person[1].name);
 
+// create dom elements for each person
 domString = "";
-
 var counter = 0;
 var outputPerson = document.getElementById("output-container");
 for (counter; counter < 5; counter++) {
-  // Give each person element a unique id
-  // outputPerson.innerHTML += `<div class="person-container" id="person-${counter}"></div>`;
 
 	var currentPerson = person[counter];
-console.log(`${currentPerson.name}`);
+  // Give each person element a unique id
+  // Grab each value from each person array object
+  //
 	domString +=			`<div class="person-container" id="person-${counter}">`;
 	domString +=				`<header>`;
 	domString +=				  `<h4> ${currentPerson.name}, ${currentPerson.title}</h4>`;
-	domString +=				`</header`;
+	domString +=				`</header>`;
 	domString +=				`<section>`;
 	domString +=					`<img src="${currentPerson.image}">`;
 	domString +=					`<p>${currentPerson.bio}</p>`;
@@ -80,18 +79,28 @@ console.log(`${currentPerson.name}`);
 	domString +=					`<h6>Death: ${currentPerson.lifespan.death}</h6>`;
 	domString +=				`</footer>`;
 	domString +=			`</div>`;
-
 }
-console.log(domString);
+//print result of domString
 outputPerson.innerHTML += domString;
 
 
 // Now personElements will have elements in it
-var personElements = document.getElementsByClassName("personContainer");
+var personElements = document.getElementsByClassName("person-container");
 console.log("personElements", personElements);
+
 // Event listeners are created
 for (var i = 0; i < personElements.length; i++) {
   personElements[i].addEventListener("click", function (event) {
     // Logic to execute when the element is clicked
+    // logging event listeners working on each created box
+    console.log("clickable persons", event)
+
+    // Since there are two layers in each div with the class "person-container", I had to make the event listener listen for a click in both layers
+	  if (event.target.parentNode.className === "person-container" || event.target.parentNode.parentNode.className === "person-container") {
+	  	// logging that all clicks in a person's box register as clicks in "person-container"
+	  	console.log("clicked in a person's div", event)
+	  }
   });
 };
+
+
