59ae996f40b61f129614c04b360b732fb38522df
Alex Sanchez <alesanchez728@gmail.com>
2017-08-22 16:00:48 -0500

    completed 1-7, and part of  8-10?

--- wysiwyg.js
+++ wysiwyg.js
@@ -10,49 +10,40 @@ var person = [{
     death: 1797
   }
 },	{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  title: "Writer",
+  name: "Seán O'Casey",
+  bio: "Seán O'Casey was an Irish dramatist and memoirist. A committed socialist, he was the first Irish playwright of note to write about the Dublin working classes.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Sean_ocasey_1924.jpg",
   lifespan: {
-    birth: 1747,
-    death: 1797
+    birth: 1880,
+    death: 1964
   }
 },	{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  title: "Secret Agent",
+  name: "George  Starr",
+  bio: "George Reginald Starr was a British mining engineer and one of the Special Operations Executive's best secret agents during World War II. He was decorated with the Distinguished Service Order, the Military Cross, and the Croix de Guerre Avec Palme. He was also made an Officer of the Légion d'honneur. The United States government awarded him the Medal of Freedom with Silver Bar.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/c/c0/George_Reginald_Starr.jpg",
   lifespan: {
-    birth: 1747,
-    death: 1797
+    birth: 1904,
+    death: 1980
   }
 },	{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  title: "Lieutenant General",
+  name: "Samuel Lomax",
+  bio: "Lieutenant General Samuel Holt Lomax was a British Army officer who commanded the 1st Division during the early battles of the First World War. He was seriously wounded by an artillery shell at the First Battle of Ypres and died of his wounds in early 1915, one of the most senior British officers to die on active service during the war.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/General_Samuel_Lomax.jpg",
   lifespan: {
-    birth: 1747,
-    death: 1797
+    birth: 1855,
+    death: 1915
   }
 },	{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  title: "Musician",
+  name: "Sufjan Stevens",
+  bio: "Sufjan Stevens is an American singer-songwriter and multi-instrumentalist. His debut album A Sun Came was released in 2000 on the Asthmatic Kitty label which he cofounded with his stepfather. He is perhaps best known for his 2005 album Illinois, which hit number one on the Billboard Top Heatseekers chart.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Sufjan_Stevens_performing_at_Pitchfork%2C_2016.jpg",
   lifespan: {
-    birth: 1747,
-    death: 1797
-  }
-},	{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
-  lifespan: {
-    birth: 1747,
-    death: 1797
+    birth: 1975,
+    death: "Can't kill Sufjan"
   }
 }];
 
@@ -71,8 +62,9 @@ for (counter; counter < 5; counter++) {
 	domString +=				  `<h4> ${currentPerson.name}, ${currentPerson.title}</h4>`;
 	domString +=				`</header>`;
 	domString +=				`<section>`;
-	domString +=					`<img src="${currentPerson.image}">`;
-	domString +=					`<p>${currentPerson.bio}</p>`;
+	domString +=					`<img src=${currentPerson.image}>`;
+	domString +=					`<p class="history">${currentPerson.bio}</p>`;
+	domString +=					`<p id="writeHere"></p>`;
 	domString +=				`</section>`;
 	domString +=				`<footer>`;
 	domString +=					`<h6>Birth: ${currentPerson.lifespan.birth}</h6>`;
@@ -86,21 +78,50 @@ outputPerson.innerHTML += domString;
 
 // Now personElements will have elements in it
 var personElements = document.getElementsByClassName("person-container");
-console.log("personElements", personElements);
-
+var inputTarget = document.getElementById("input-field");
+var history = document.getElementsByClassName("history");
+var reWriteHistory = document.getElementById("writeHere");
+var global;
 // Event listeners are created
 for (var i = 0; i < personElements.length; i++) {
   personElements[i].addEventListener("click", function (event) {
-    // Logic to execute when the element is clicked
-    // logging event listeners working on each created box
-    console.log("clickable persons", event)
+	  // logging that all clicks in a person's box register as clicks in "person-container"
+  	console.log("clicked in a person's div", event);
+  	
+  	// add border to selected container
+	 	dotBorder(event);
 
-    // Since there are two layers in each div with the class "person-container", I had to make the event listener listen for a click in both layers
-	  if (event.target.parentNode.className === "person-container" || event.target.parentNode.parentNode.className === "person-container") {
-	  	// logging that all clicks in a person's box register as clicks in "person-container"
-	  	console.log("clicked in a person's div", event)
-	  }
+	 	// hides OG history
+ 		// puts focus on inputTarget
+ 		// allows all typed text to be in place of OG history
+ 		// when the personContainer is blurred, text reverts to normal
+	 	changeHistory(event);
   });
 };
+
+
+function dotBorder(event) {
+  global = event.currentTarget;
+  global.classList.toggle("border");
+}
+function changeHistory(event) {
+  // To be used to change the p text
+  global.childNodes[1].childNodes[1].classList.toggle("hidden");
+  
+  inputTarget.focus();
+
+	inputTarget.addEventListener("keyup", function(e){
+		console.log(e.target.value)
+		reWriteHistory.innerHTML = `${e.target.value}`;
+	});
+
+	inputTarget.addEventListener('keypress', function(event){
+		if(event.key === 'Enter') {
+			reWriteHistory.innerHTML = ``;
+			inputTarget.value = "";
+		  global.childNodes[1].childNodes[1].classList.toggle("hidden");
+		}
+	});
+}
 
 
